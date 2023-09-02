125dcb15e946dc0d5a737bf561bd2b89532bbbe8
Fahim Murshed <437559+murshed@users.noreply.github.com>
2023-09-03 00:07:47 +0600

    Add files via upload

---
+++ assets/en.js
@@ -0,0 +1,83 @@
+var cH={"cb":"<span class=\"english\">2.1.0 Alpha 1</span>","ba":"<span class=\"english\">September 18, 2007</span>","de":"<span class=\"english\">Murshed's Unicode layout and converter</span>","cF":"<span class=\"english\">Online application to write and convert conventional Bangla text to unicode. (<a href=\"http://sourceforge.net/project/showfiles.php?group_id=177292\">Download<a>, <a href=\"#section_license_info\">License</a>)</span>","cj":"<span class=\"english\">Developers: <a href=\"mailto:udvranto@yahoo.com\">S M Mahbub Murshed</a>, <a href=\"mailto:arup.kamal@gmail.com\">Arup Kamal</a></span>","bQ":"<span class=\"english\">You can write Unicode Bangla in the first textbox after selecting a layout</span>","ds":"<span class=\"english\">English</span>","dv":"<span class=\"english\">Unijoy</span>","dp":"<span class=\"english\">Probhat</span>","dq":"<span class=\"english\">Somewhere-in</span>","dr":"<span class=\"english\">Avro</span>","dz":"<span class=\"english\">Bornosoft</span>","cG":"<span class=\"english\">show help</span>","cD":"<span class=\"english\">hide help</span>","ca":"<img src=images/unicodeen.jpg></img>","bO":"<span class=\"english\">To convert conventional Bengali to Unicode copy the text in the text box below and select what kind of text it is. Then click \"ag into Unicode Editor\" to get the converted text in the text box above.</span>","dK":"<span class=\"english\">Bijoy</span>","dL":"<span class=\"english\"><a href=fonts/Sushree.ttf>Somewhere-in Text</a></span>","dI":"<span class=\"english\"><a href=fonts/boishakhi.ttf>Boisakhi</a></span>","dJ":"<span class=\"english\"><a href=fonts/BngsAlpn.ttf>Bangsee</a></span>","dW":"<span class=\"english\"><a href=fonts/falgun.ttf>Bornosoft</a></span>","dN":"<span class=\"english\">Phonetic</span>","dO":"<span class=\"english\">HTML Safe Hex</span>","dT":"<span class=\"english\">HTML Safe Dec</span>","bk":"<img src=images/puronoen.jpg></img>","dc":"ag into Unicode Editor","db":"Convert Back to Conventional Bangla","cu":"Refresh","bw":"<span class=\"english\">Key Pressed: </span>","cE":"<span class=\"english\">Bangla Web Tools Homepage</span>","bL":"<span class=\"english\">The project is aimed to develop Unicode based Bangla (Bengali) tools to help web development. Tools like keyboard interpreter according to Bangla, Avro Phonetic layout etc. and converters to convert from conventional bangla (ASCII based) to Unicode &amp; vice versa has been created in this project.  <a href=\"http://sourceforge.net/projects/bnwebtools\">You will find the proeject page on sourceforge here.</a></span>","bH":"<span class=\"english\"><strong>BNWebtools license</strong></span>","bR":"<p><span class=\"english\">From September 18, 2007 (version 2.1.0), this project is released under two different licenses.\
+<ul class=\"english\">\
+<li> If you want to use this project in a personal or non-commercial project, you can use it under \
+<a href=\"http://www.gnu.org/licenses/gpl.html\">GPL 3</a> license.</li>\
+<li> But for commercial project you have to purchase an enterprise license.\
+For more information about enterprise license contact <a href=\"mailto:udvranto@yahoo.com\">S M Mahbub Murshed</a> </li></ul></span></p>","bG":"<p class=\"english\"><strong>Keyboard shortcuts:</strong><br>\
+<ul>\
+<li>To type English press Ctl+Alt+E or Escape.</li>\
+<li>To type Bangla in Somewhere in Phonetic layout press Ctl+Alt+P.</li>\
+<li>To type Bangla in Avro Phonetic layout press Ctl+Alt+A. Note, this is still in development phase.</li>\
+<li>To type Bangla in Probhat layout press Ctl+Alt+V.</li>\
+<li>To type Bangla in Bornosoft Accent layout press Ctl+Alt+O.</li>\
+</ul></p>","bp":"<p class=\"english\"><strong>Conversion from/to conventional Bangla (ASCII based):</strong><br>\
+<ul class=\"english\">\
+<li><i>Conventional Bangla to Unicode</i><br>\
+If you want to convert from Bijoy 2001, Somewhere-in-text or Boisakhi \
+Bangla to Unicode Bangla paste the text in the second text box at the\
+bottom. Select the appropriate option in top of the second text box\
+and then press \"<i>ag into Unicode Editor</i>\" Button.</li><br>\
+\
+<li><i>Unicode to conventional Bangla</i><br>\
+If you want to convert to Bijoy 2001, Somewhere-in-text or Boisakhi\
+Bangla from Unicode Bangla paste the Unicode text in the first text\
+box or type it in. Then select the appropriate conventional Bangla text mode you want\
+from the list at the top of the second text box as you did for conventioanl Bangla\
+conversion. Then click \"<i>Convert Back to Conventional Bangla</i>\" to convert to conventional Bangla format.</li><br>\
+\
+<li><i>Conventioanl Bangla to another conventional Bangla format</i><br>\
+If you want to convert from Bijoy to Boisakhi or vice versa.\
+First convert it to Unicode and then convert it back again in Boisakhi or Bijoy.</li>\
+</ul></p>","bZ":"<p class=\"english\"><strong>Other similar tools</strong><br>\
+<ul class=\"english\">\
+<li><i>Online Keyboard Layouts</i>\
+  <ul>\
+    <li><a href=\"http://bangla.org.bd/\">Sajed Chowdhury\'s rich text and Lekho Layout</a></li>\
+    <li><a href=\"http://www.stat.wisc.edu/~deepayan/Bengali/WebPage/Contrib/BanglaInput.html\">Deepayan Sarkar\'s Lekho Layout</a></li>\
+    <li><a href=\"http://phpxperts.net/unijoy/example.htm\">Hasin Hyder\'s Unijoy Layout</a></li>\
+    <li><a href=\"http://phpxperts.net/phonetic/example.html\">Hasin Hyder\'s Phonetic Layout</a></li>\
+  </ul>\
+</li>\
+\
+<li><i>Online Converters</i>\
+  <ul>\
+    <li><a href=\"http://converter.banglacomputing.net/convertBij2Boi.php\">Ashar Alo\'s Bijoy to Boisakhi Converter</a></li>\
+    <li><a href=\"http://converter.banglacomputing.net/convertBA2Boi.php\">Ashar Alo\'s Bangshi Alpona to Boisakhi Converter</a></li>\
+    <li><a href=\"http://converter.banglacomputing.net/convertBoi2Bij.php\">Ashar Alo\'s Boisakhi to Bijoy Converter</a></li>\
+    <li><a href=\"http://converter.banglacomputing.net/convertUni2Bij.php\">Ashar Alo\'s Unicode to Bijoy Converter</a></li>\
+    <li><a href=\"http://converter.banglacomputing.net/convertBoi2Uni.php\">Ashar Alo\'s Boisakhi to Unicode Converter</a></li>\
+  </ul>\
+</li>\
+</ul></p>","cW":"<p class=\"english\"><strong>Thanks to:</strong><br>\
+<ul class=\"english\">\
+<li><i><a href=\"http://konfusias.blogspot.com/\">Konfusius</a></i><br>\
+For testing and providing valuable suggestions during its development.</li>\
+\
+<li><i><a href=\"http://royesoye.blogspot.com/\">Himu</a></i><br>\
+For testing and providing valuable suggestions during its development.</li>\
+\
+<li><i><a href=\"http://www.somewhereinblog.net/asharaloblog\">Ashar Alo</a></i><br>\
+For providing valuable suggestions during its development.</li>\
+\
+<li><i><a href=\"http://www.bd.net/\">Sajed Chowdhury</a></i><br>\
+For providing valuable suggestions.</li>\
+\
+<li><i><a href=\"http://www.somewhereinblog.net/kowshikblog\">Kowshik</a></i><br>\
+For user interface suggestions and promoting.</li>\
+\
+<li><i><a href=\"http://www.somewhereinblog.net/babelblog\">Timur</a></i><br>\
+For finding difficult and hard to find bugs.</li>\
+\
+<li><i><a href=\"http://www.somewhereinblog.net/hussainuzzamanblog\">Miah M. Hussainuzzaman</a></i><br>\
+For finding a lot of bugs.</li>\
+\
+<li><i><a href=\"http://www.somewhereinblog.net/rumanblog\">Dhusor Godhuli</a></i><br>\
+For promoting.</li>\
+\
+<li><i><a href=\"http://www.mukto-mona.com\">Avijit Roy</a></i>\
+<br>\
+For helping with bornosoft converter.</li>\
+\
+<li><i>You</i><br>\
+For using. :-)</li>\
+</ul></p>"}; 
